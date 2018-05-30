6c22ce958480460d1af6968d70b70bd1a68f284a
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-05-30 15:29:09 +0100

    add styled-component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 import './css/index.css'
 
+const Text = styled.p.attrs({
+  className: 'font-5 ttc sans-serif black',
+})``
+
 const App = () => {
-  return <p>hello, world</p>
+  return <Text>hello, world</Text>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
