c5235565e8327ffdaa9ae7f385df56312454be86
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-05-30 15:23:36 +0100

    react hello world

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import './css/index.css'
+
+const App = () => {
+  return <p>hello, world</p>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
